7dd57995a037aa215383de9946be4eebd85c4e0b
Matthew <matthew.t.kides@cox.net>
2015-05-12 00:19:23 -0700

    Testing
    Made a test room and implemented basic movement and camera controls.
    Only works with a controller.

---
+++ Assets/Scripts/Camera/CameraMovement.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMovement : MonoBehaviour
+{
+    public GameObject target; 
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.DrawRay(transform.position, new Vector3(transform.forward.normalized.x, 0, transform.forward.normalized.z));
+        if (Input.GetAxis("CameraHorizontal") > 0)
+        {
+            transform.RotateAround(target.transform.position, Vector3.up, 85 * Input.GetAxis("CameraHorizontal") * Time.deltaTime);
+        }
+        else if (Input.GetAxis("CameraHorizontal") < 0)
+        {
+            transform.RotateAround(target.transform.position, Vector3.up, 85 * Input.GetAxis("CameraHorizontal") * Time.deltaTime);
+        }
+
+        if (Input.GetAxis("CameraVertical") > 0 && transform.eulerAngles.x < 80)
+        {
+            transform.RotateAround(target.transform.position, transform.right, 65 * Input.GetAxis("CameraVertical") * Time.deltaTime);
+        }
+        else if (Input.GetAxis("CameraVertical") < 0 && transform.eulerAngles.x > 10)
+        {
+            transform.RotateAround(target.transform.position, transform.right, 65 * Input.GetAxis("CameraVertical") * Time.deltaTime);
+        }
+    }
+}
---
+++ Assets/Scripts/Objects/Player.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour
+{
+    public Camera gCamera;
+    Rigidbody playerPhysics;
+    float moveSpeed;
+    float maxSpeed;
+    float jumpForce;
+    int jumpCount;
+    int maxJumps;
+
+    // Use this for initialization
+    void Start()
+    {
+        playerPhysics = GetComponent<Rigidbody>();
+        moveSpeed = 11.0f;
+        maxSpeed = 3.0f;
+        jumpForce = 4.0f;
+        jumpCount = 1;
+        maxJumps = 1;
+        gCamera = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump") && jumpCount > 0) 
+        {
+            playerPhysics.AddForce(new Vector3(0, 1, 0) * jumpForce * Time.fixedDeltaTime, ForceMode.Impulse);
+            jumpCount--;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetAxis("Vertical") > 0 && playerPhysics.velocity.magnitude < maxSpeed)
+        {
+            playerPhysics.AddForceAtPosition(new Vector3(gCamera.transform.forward.x, 0, gCamera.transform.forward.z).normalized * moveSpeed * Time.fixedDeltaTime * Input.GetAxis("Vertical"), new Vector3(transform.position.x, transform.position.y + (0.35f * transform.localScale.y), transform.position.z - (0.5f * transform.localScale.z)), ForceMode.Force);
+        }
+        else if (Input.GetAxis("Vertical") < 0 && playerPhysics.velocity.magnitude < maxSpeed)
+        {
+            playerPhysics.AddForceAtPosition(new Vector3(gCamera.transform.forward.x, 0, gCamera.transform.forward.z).normalized * moveSpeed * Time.fixedDeltaTime * Input.GetAxis("Vertical"), new Vector3(transform.position.x, transform.position.y + (0.35f * transform.localScale.y), transform.position.z + (0.5f * transform.localScale.z)), ForceMode.Force);
+        }
+
+        if (Input.GetAxis("Horizontal") > 0 && playerPhysics.velocity.magnitude < maxSpeed)
+        {
+            playerPhysics.AddForceAtPosition(gCamera.transform.right * moveSpeed * Time.fixedDeltaTime * Input.GetAxis("Horizontal"), new Vector3(transform.position.x - (0.5f * transform.localScale.x), transform.position.y + (0.35f * transform.localScale.y), transform.position.z), ForceMode.Force);
+        }
+        else if (Input.GetAxis("Horizontal") < 0 && playerPhysics.velocity.magnitude < maxSpeed)
+        {
+            playerPhysics.AddForceAtPosition(gCamera.transform.right * moveSpeed * Time.fixedDeltaTime * Input.GetAxis("Horizontal"), new Vector3(transform.position.x + (0.5f * transform.localScale.x), transform.position.y + (0.35f * transform.localScale.y), transform.position.z), ForceMode.Force);
+        } 
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        Ray ray = new Ray(transform.position, -(Vector3.up));
+        if(Physics.Raycast(ray, 0.2f, 1 << 8))
+        {
+            jumpCount = maxJumps;
+        }
+    }
+
+}
