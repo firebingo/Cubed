8a428cbaed94fa667db97e939d3a311fd70afeae
Matthew <matthew.t.kides@cox.net>
2015-05-24 23:52:10 -0700

    Powerups
    Added powerups to enable the ice and fire shields.

---
+++ Assets/Scripts/Objects/FirePowerUp.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FirePowerUp : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        //if the player hits the powerup, enable the global powerup use.
+        if (other.gameObject.name == "Player" || other.gameObject.name == "Player(Clone)")
+        {
+            GameController.gameMaster.playerCanUseFireShield = true;
+            Destroy(this.gameObject);
+        }
+    }
+}
