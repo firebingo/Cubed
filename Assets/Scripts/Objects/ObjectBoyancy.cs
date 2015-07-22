b9e32dc023cdf841d630ab65d2c54a6e0302e09f
Matthew <matthew.t.kides@cox.net>
2015-07-22 15:54:29 -0700

    Island/Ship Assets
    Added assets for the island and ship fragments. Also made a script for
    object boyancy on water.

---
+++ Assets/Scripts/Objects/ObjectBoyancy.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectBoyancy : MonoBehaviour
+{
+    public GameObject waterObject;
+    Rigidbody objectPhysics;
+    float gravityMul;
+
+    // Use this for initialization
+    void Start()
+    {
+        objectPhysics = GetComponent<Rigidbody>();
+        gravityMul = 1.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (transform.position.y > waterObject.transform.position.y)
+            gravityMul = 1.0f;
+        else if (transform.position.y < waterObject.transform.position.y)
+            gravityMul = -1.0f;
+
+        if (transform.position.y > waterObject.transform.position.y - 0.1f && transform.position.y < waterObject.transform.position.y + 0.1f)
+            gravityMul = 0;
+
+        //Gravity, be sure to disable the rigidbody's gravity so it just uses this.
+        objectPhysics.AddForce(Physics.gravity * objectPhysics.mass * gravityMul);
+    }
+}
